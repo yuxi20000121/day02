b0bde9f788951c33e98240b06e5a58728e371eb7
俞 <yuxi_20000121@163.com>
2020-09-06 00:16:24 +0800

    初始化

---
+++ src/com/offcn/Deom01/Deom01.java
@@ -0,0 +1,13 @@
+package com.offcn.Deom01;
+
+public class Deom01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+
+    }
+}
