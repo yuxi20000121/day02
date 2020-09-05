791d46d95467f3942f88f232c44aad9d6bf5bcfc
俞 <yuxi_20000121@163.com>
2020-09-06 00:41:00 +0800

    初始化

--- src/com/offcn/Deom01/Deom01.java
+++ src/com/offcn/Deom01/Deom01.java
@@ -5,9 +5,9 @@ public class Deom01 {
     public static void main(String[] args) {
 
 
-        System.out.println("123213");
-        System.out.println("123213");
-        System.out.println("123213");
+        System.out.println("12");
+        System.out.println("");
+        System.out.println("");
 
 
     }
