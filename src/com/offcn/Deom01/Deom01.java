e039a2a4129d3fa9eb14a7f4858af7f3c1d8d90d
俞 <yuxi_20000121@163.com>
2020-09-06 00:28:37 +0800

    初始化

--- src/com/offcn/Deom01/Deom01.java
+++ src/com/offcn/Deom01/Deom01.java
@@ -4,10 +4,13 @@ public class Deom01 {
 
     public static void main(String[] args) {
 
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-
+        System.out.println("分支来了");
+        System.out.println("分支来了");
+        System.out.println("分支来了");
+        System.out.println("分支来了");
+        System.out.println("分支来了");
+        System.out.println("分支来了");
+        System.out.println("分支来了");
+        System.out.println("分支来了");
     }
 }
